13aca331eb8705ca30f11da8f560851d38346c24
Krisjrj <398679378@qq.com>
2021-02-02 16:37:50 +0800

    2_1

--- 2_1/leetcode.c
+++ 2_1/leetcode.c
@@ -8,13 +8,13 @@
  */
 
 
-struct ListNode* removeElements(struct ListNode* head, int val) {
-    if (head == NULL)
-        return NULL;
-    head->next = removeElements(head->next, val);
-    return head->val == val ? head->next : head;
-
-}
+//struct ListNode* removeElements(struct ListNode* head, int val) {
+//    if (head == NULL)
+//        return NULL;
+//    head->next = removeElements(head->next, val);
+//    return head->val == val ? head->next : head;
+//
+//}
 //C++
 /**
  * Definition for singly-linked list.
@@ -26,17 +26,17 @@ struct ListNode* removeElements(struct ListNode* head, int val) {
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-
-    ListNode* removeElements(ListNode* head, int val) {
-        int count = 0;
-        if (head == nullptr)
-            return nullptr;
-        head->next = removeElements(head->next, val);
-        if (head->val == val)
-            return head->next;
-        else
-            return head;
-    }
-};
+//class Solution {
+//public:
+//
+//    ListNode* removeElements(ListNode* head, int val) {
+//        int count = 0;
+//        if (head == nullptr)
+//            return nullptr;
+//        head->next = removeElements(head->next, val);
+//        if (head->val == val)
+//            return head->next;
+//        else
+//            return head;
+//    }
+//};
--- 2_1/test.cpp
+++ 2_1/test.cpp
@@ -1,21 +1,21 @@
 #include<iostream>
 using namespace std;
 
-//函数重载
-int Add(int left, int right)
-{
-	return left + right;
-}
- 
-double Add(double left, double right)
-{
-	return left + right;
-}
-
-long Add(long left, long right)
-{
-	return left + right;
-}
+////函数重载
+//int Add(int left, int right)
+//{
+//	return left + right;
+//}
+// 
+//double Add(double left, double right)
+//{
+//	return left + right;
+//}
+//
+//long Add(long left, long right)
+//{
+//	return left + right;
+//}
 
 //int main()
 //{
@@ -24,11 +24,77 @@ long Add(long left, long right)
 //	Add(10L, 20L);
 //	return 0;
 //}
+//int main()
+//{
+//	int a = 10;
+//	int& b = a;//这里的&是引用，不是取地址
+//	a = 20;
+//	b = 30;
+//	return 0;
+//}
+
+//int main()
+//{
+//	//指针和引用在初始化赋值时，权限可以缩小，不能放大
+//	const int a = 10;
+//	//int& b = a;
+//	const int& b = a;
+//
+//	int c = 10;
+//	const int& d = c;
+//	const int& r = 10;
+//	
+//	//整形和浮点数之间互相隐式类型转换
+//	double dd = 2.22;
+//	int i = dd;
+//	
+//	const int& ri = dd;
+//	return 0;
+//
+//}
+//void Swap(int& x, int& y)//引用
+//{
+//	int tmp = x;
+//	x = y;
+//	y = tmp;
+//}
+//void Swap(int* px, int* py)//指针
+//{
+//	int tmp = *px;
+//	*px = *py;
+//	*py = tmp;
+//}
+//int main()
+//{
+//	int a = 0, b = 1;
+//	Swap(&a, &b);
+//	Swap(a, b);
+//	return 0;
+//}
+//int Count()
+//{
+//	static int n = 0;
+//	n++;
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int ret = Count();
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+int& Add(int a, int b)
+{
+	int c = a + b;
+	return c;
+}
+
 int main()
 {
-	int a = 10;
-	int& b = a;
-	a = 20;
-	b = 30;
+	int& ret = Add(1, 2);
+	Add(3, 4);
+	cout << "Add(1, 2) = " << ret << endl;
 	return 0;
 }
